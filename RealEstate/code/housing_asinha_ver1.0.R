ac93aad849a10a86c0e6c42fac3d8109ba99e160
Anugraha Sinha <anugraha.sinha@gmail.com>
2017-09-04 13:03:42 +0530

    initial commit for R code.

---
+++ RealEstate/code/housing_asinha_ver1.0.R
@@ -0,0 +1,24 @@
+
+
+
+
+# Setup #
+setwd("C:/Users/anugraha.sinha/OneDrive/Documents/Kaggle_work_git_repo/kaggle/RealEstate/data")
+
+
+## Data Sourcing ##
+housing_train <- read.csv("housing_train.csv",stringsAsFactors = FALSE)
+housing_test <- read.csv("housing_test.csv",stringsAsFactors = FALSE)
+
+# Business Understanding #
+# We need predict the house prices, based on the data sets given.#
+
+# Data Understanding #
+
+
+# Data Preparation #
+# Model Preparation #
+# Model Evaluation #
+# Model Deployement #
+
+# Data Understanding #
