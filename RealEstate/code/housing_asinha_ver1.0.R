c2bb647b89999e6df28d145ea1f5da29955e9129
Anugraha Sinha <anugraha.sinha@gmail.com>
2017-09-04 16:43:24 +0530

    Addition of more code.

--- RealEstate/code/housing_asinha_ver1.0.R
+++ RealEstate/code/housing_asinha_ver1.0.R
@@ -1,5 +1,5 @@
-
-
+library(dplyr)
+library(ggplot2)
 
 
 # Setup #
@@ -14,7 +14,32 @@ housing_test <- read.csv("housing_test.csv",stringsAsFactors = FALSE)
 # We need predict the house prices, based on the data sets given.#
 
 # Data Understanding #
+# Lets keep the price column of train data set separately and do a merge of this later on #
+housing_train_price <- housing_train$Price
 
+housing_train <- housing_train %>% select(-Price)
+
+# Lets add a column called "DataType" = Train and "DataType" = Test for both the train
+# and test data set and merge the 2 data frames
+
+housing_train$DataType <- "Train"
+housing_test$DataType <- "Test"
+
+
+housing_all <- rbind(housing_train,housing_test)
+
+View(housing_all)
+
+# NA Analysis #
+sapply(housing_all, function(x) sum(is.na(x)))
+
+
+housing_cor_check <- housing_all %>% 
+  select(Rooms,Distance,Bedroom2,Bathroom,Car,Landsize,BuildingArea,YearBuilt) %>% 
+  filter(!is.na(Bedroom2),!is.na(Bathroom),!is.na(Car),!is.na(Landsize),
+         !is.na(BuildingArea),!is.na(YearBuilt))
+
+cor(housing_cor_check[,-8])
 
 # Data Preparation #
 # Model Preparation #
